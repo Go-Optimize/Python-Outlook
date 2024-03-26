df92af46795767b66f7fb4d9543be0e1d2fe5e7c
Lets-Optimize <164369846+Go-Optimize@users.noreply.github.com>
2024-03-26 12:04:27 -0300

    Update pylint envio_email.py

--- dist_low_code/envio_email.py
+++ dist_low_code/envio_email.py
@@ -1,30 +1,39 @@
+"""
+Este módulo contém um exemplo de utilização da função enviar_email do módulo func_outlook_python.
+Pylint: 0 -> 10
+"""
+
 # Importações
 import func_outlook_python
 
 #* FORMULÁRIO DE ENVIO ====================================
 # Lista de destinários e cópias
-remetente = "seu_email@exemplo.com" # Insira o e-mail do qual você deseja enviar
-destinatarios = ["dest_1@exemplo.com", "dest_2@exemplo.com", "dest_3@exemplo.com"] # Insira os e-mails dos destinatários
-cc = ["cc_1@exemplo.com", "cc_2@exemplo.com"] # Insira os e-mails dos destinatários em cópia (caso não tenha nenhum deixar colchetes vazios [])
+
+# Insira o e-mail do qual você deseja enviar
+REMENTENTE = "seu_email@exemplo.com"
+# Insira os e-mails dos destinatários
+DESTINATARIOS = ["dest_1@exemplo.com", "dest_2@exemplo.com", "dest_3@exemplo.com"]
+# Insira os e-mails dos destinatários em cópia (caso não tenha nenhum deixar colchetes vazios [])
+CC = ["cc_1@exemplo.com", "cc_2@exemplo.com"]
 
 # Estrutura do e-mail
-titulo = "[nome do relatório]"
-corpo = "Prezados bom dia! \nSegue em anexo [nome do relatório] atualizado até o dia [data]. \nQualquer dúvida estou à disposição."
+TITULO = "[nome do relatório]"
+CORPO = "Prezados bom dia! \nSegue em anexo [nome do relatório] atualizado até o dia [data]."
 
 # Caminho dos anexos
-print = r'C:\Users\Usuario\Entregas\Relatorio\print.png'
-assinatura = r'C:\Users\Usuario\Entregas\Relatorio\assinatura.png'
-anexo = r'C:\Users\Usuario\Entregas\Relatorio\anexo.xlsx'
+SCREENSHOT = r'C:\Users\Usuario\Entregas\Relatorio\print.png'
+ASSINATURA = r'C:\Users\Usuario\Entregas\Relatorio\assinatura.png'
+ANEXO = r'C:\Users\Usuario\Entregas\Relatorio\anexo.xlsx'
 # =========================================================
 
 # ENVIO ===================================================
 func_outlook_python.enviar_email(
-    remetente = remetente,
-    destinatarios = destinatarios, 
-    cc = cc,
-    titulo = titulo,
-    assunto = corpo,
-    caminho_print = print,
-    caminho_assinatura = assinatura,
-    caminho_anexo = anexo)
+    remetente = REMENTENTE,
+    destinatarios = DESTINATARIOS,
+    cc = CC,
+    titulo = TITULO,
+    assunto = CORPO,
+    caminho_screenshot = SCREENSHOT,
+    caminho_assinatura = ASSINATURA,
+    caminho_anexo = ANEXO)
 # =========================================================
