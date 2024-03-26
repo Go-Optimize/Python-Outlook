2baa45323751cd4dfe3d764aab7f8f5b2a12b100
Lets-Optimize <164369846+Go-Optimize@users.noreply.github.com>
2024-03-26 14:56:46 -0300

    Update func_outlook_python.py

--- dist_low_code/func_outlook_python.py
+++ dist_low_code/func_outlook_python.py
@@ -26,52 +26,56 @@ def enviar_email(remetente, destinatarios, cc, titulo, assunto, caminho_screensh
     Retorno:
     none: Esta função realiza uma ação e não gera retorno.
     """
-    # Criar uma instância do Outlook
-    outlook = win32com.client.Dispatch('Outlook.Application')
+    try:
+        # Criar uma instância do Outlook
+        outlook = win32com.client.Dispatch('Outlook.Application')
 
-    # Criar um e-mail
-    email = outlook.CreateItem(0)  # 0 representa o tipo de item de e-mail
+        # Criar um e-mail
+        email = outlook.CreateItem(0)  # 0 representa o tipo de item de e-mail
 
-    # Acessar o perfil desejado pelo endereço de e-mail
-    profile = None
-    namespace = outlook.GetNamespace("MAPI")
-    for acc in namespace.Accounts:
-        if acc.SmtpAddress == remetente:
-            profile = acc
-            break
+        # Acessar o perfil desejado pelo endereço de e-mail
+        profile = None
+        namespace = outlook.GetNamespace("MAPI")
+        for acc in namespace.Accounts:
+            if acc.SmtpAddress == remetente:
+                profile = acc
+                break
 
-    if profile is not None:
-        email._oleobj_.Invoke(*(64209, 0, 8, 0, profile)) # Configurar o perfil do remetente
-    else:
-        print("Perfil não encontrado")
+        if profile is not None:
+            email._oleobj_.Invoke(*(64209, 0, 8, 0, profile)) # Configurar o perfil do remetente
+        else:
+            print("Perfil não encontrado")
 
-    # Configurar os campos do e-mail
-    email.To = ';'.join(destinatarios)
-    email.CC = ';'.join(cc)
+        # Configurar os campos do e-mail
+        email.To = ';'.join(destinatarios)
+        email.CC = ';'.join(cc)
 
-    # Conteúdo do E-Mail
-    email.Subject = titulo
-    email.Body = assunto
+        # Conteúdo do E-Mail
+        email.Subject = titulo
+        email.Body = assunto
 
-    # Anexar imagem
-    screenshot_attachment = email.Attachments.Add(caminho_screenshot)
-    screenshot_attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001E", "imagem")
+        # Anexar imagem
+        screenshot_attachment = email.Attachments.Add(caminho_screenshot)
+        screenshot_attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001E", "imagem")
 
-    # Configurar a imagem como incorporada
-    email.HTMLBody = email.HTMLBody + '<img src="cid:imagem">'
+        # Configurar a imagem como incorporada
+        email.HTMLBody += '<img src="cid:imagem">'
 
-    # Adicionar linha "Atenciosamente,"
-    email.HTMLBody += "<br><br>Atenciosamente,"
+        # Adicionar linha "Atenciosamente,"
+        email.HTMLBody += "<br><br>Atenciosamente,"
 
-    # Anexar imagem da assinatura
-    assinatura_attachment = email.Attachments.Add(caminho_assinatura)
-    assinatura_attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001E", "assinatura")
+        # Anexar imagem da assinatura
+        assinatura_attachment = email.Attachments.Add(caminho_assinatura)
+        assinatura_attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001E", "assinatura")
 
-    # Configurar a imagem da assinatura como incorporada
-    email.HTMLBody += '<br><img src="cid:assinatura">'
+        # Configurar a imagem da assinatura como incorporada
+        email.HTMLBody += '<br><img src="cid:assinatura">'
 
-    # Anexar arquivo de planilha
-    email.Attachments.Add(caminho_anexo)
+        # Anexar arquivo de planilha
+        email.Attachments.Add(caminho_anexo)
 
-    # Enviar o e-mail
-    email.Send()
+        # Enviar o e-mail
+        email.Send()
+
+    except Exception as e:
+        input("Erro ao enviar e-mail. Erro: " + str(e))
